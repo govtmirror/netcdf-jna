49a8124e015f3686c554893b08f2ca5b6e0f9e38
tkunicki <tkunicki@622dc1fb-2224-0410-ab72-dc85a3975e1a>
2010-09-18 05:21:58 +0000

    Augmented XType enumeration with lookups and code reporting. Cleaned up compound attribute generation code
    git-svn-id: https://cida-svn.er.usgs.gov/repos/dev/usgs/netcdf/netcdf-jna/trunk@11320 622dc1fb-2224-0410-ab72-dc85a3975e1a

--- src/main/java/edu/unidata/ucar/netcdf/jna/NCUtil.java
+++ src/main/java/edu/unidata/ucar/netcdf/jna/NCUtil.java
@@ -1,23 +1,16 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.unidata.ucar.netcdf.jna;
 
+import java.nio.Buffer;
 import com.sun.jna.NativeLong;
 import com.sun.jna.ptr.IntByReference;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.DoubleBuffer;
-import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
-import java.nio.LongBuffer;
-import java.nio.ShortBuffer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static edu.unidata.ucar.netcdf.jna.NC.*;
+import static java.nio.ByteOrder.nativeOrder;
 
 /**
  *
@@ -66,124 +59,69 @@ public class NCUtil {
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, byte[] values) {
-        int ncStatus;
-        IntByReference iRef = new IntByReference();
-        int size = values.length * 1;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
-        int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 1), NC_BYTE);
-            status(ncStatus);
-        }
-        ByteBuffer buffer = ByteBuffer.allocateDirect(size);
-        for (byte value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        Buffer buffer = allocateByteBuffer(size).put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, short[] values) {
-        int ncStatus;
-        IntByReference iRef = new IntByReference();
-        int size = values.length * 2;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
-        int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 2), NC_SHORT);
-            status(ncStatus);
-        }
-        ShortBuffer buffer = ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder()).asShortBuffer();
-        for (short value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        Buffer buffer = allocateByteBuffer(size).asShortBuffer().put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, int[] values) {
-        int ncStatus;
-        IntByReference iRef = new IntByReference();
-        int size = values.length * 4;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
-        int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 4), NC_INT);
-            status(ncStatus);
-        }
-        IntBuffer buffer = ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder()).asIntBuffer();
-        for (int value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        Buffer buffer = allocateByteBuffer(size).asIntBuffer().put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, long[] values) {
-        int ncStatus;
-        IntByReference iRef = new IntByReference();
-        int size = values.length * 8;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
-        int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 8), NC_INT64);
-            status(ncStatus);
-        }
-        LongBuffer buffer = ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder()).asLongBuffer();
-        for (long value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        Buffer buffer = allocateByteBuffer(size).asLongBuffer().put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, float[] values) {
-        int ncStatus;
-        IntByReference iRef = new IntByReference();
-        int size = values.length * 4;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
-        int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 4), NC_FLOAT);
-            status(ncStatus);
-        }
-        FloatBuffer buffer = ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder()).asFloatBuffer();
-        for (float value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        Buffer buffer = allocateByteBuffer(size).asFloatBuffer().put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
     }
 
     public static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, double[] values) {
-        int ncStatus;
+		XType xtype = XType.findXType(values);
+		long size = xtype.getSizeBytes(values.length);
+        DoubleBuffer buffer = allocateByteBuffer(size).asDoubleBuffer();
+		buffer.put(values).rewind();
+        generateCompoundAttributes(ncId, varId, cname, aname, fields, xtype, buffer);
+    }
+
+	// if visibility increased, one must check that buffer is direct and order is native.
+	private static void generateCompoundAttributes(int ncId, int varId, String cname, String aname, String[] fields, XType xtype, Buffer buffer) {
+		int ncStatus;
         IntByReference iRef = new IntByReference();
-        int size = values.length * 8;
-        ncStatus = nc_def_compound(ncId, new NativeLong(size), cname, iRef);
-        status(ncStatus);
+		int count = buffer.limit() - buffer.position();
+		long sizeBytes =  xtype.getSizeBytes(count);
+        ncStatus = nc_def_compound(ncId, new NativeLong(sizeBytes), cname, iRef); status(ncStatus);
         int typeid = iRef.getValue();
-        for (int index = 0; index < values.length; ++index) {
-            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * 8), NC_DOUBLE);
+        for (int index = 0; index < count; ++index) {
+            ncStatus = nc_insert_compound(ncId, typeid, fields[index], new NativeLong(index * xtype.sizeBytes), xtype.code);
             status(ncStatus);
         }
-        DoubleBuffer buffer = ByteBuffer.allocateDirect(size).order(ByteOrder.nativeOrder()).asDoubleBuffer();
-        for (double value : values) {
-            buffer.put(value);
-        }
-        buffer.rewind();
-        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer);
-        status(ncStatus);
+        ncStatus = nc_put_att(ncId, varId, aname, typeid, new NativeLong(1), buffer); status(ncStatus);
     }
+
+	private static ByteBuffer allocateByteBuffer(long size) {
+		if (size > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("can't allocate " + size + " bytes with this method, use JNA Memory instead");
+		}
+		return ByteBuffer.allocateDirect((int)size).order(nativeOrder());
+	}
 
 	public enum XType {
 		NC_NAT(0, -1),		/* NAT = 'Not A Type' (c.f. NaN) */
@@ -201,16 +139,31 @@ public class NCUtil {
 		NC_UINT64(11, 8),	/* unsigned 8-byte int */
 		NC_STRING(12, -1);  /* string */
 
-		int xtype;
-		int sizeBytes;
+		final int code;
+		final int sizeBytes;
 
 		XType (int xtype, int sizeBytes) {
-			this.xtype = xtype;
+			this.code = xtype;
 			this.sizeBytes = sizeBytes;
 		}
 
 		public int getSizeBytes() {
 			return sizeBytes;
+		}
+
+		public long getSizeBytes(int count) {
+			if (count < 0) {
+				throw new IllegalArgumentException("count parameter < 0");
+			}
+			if (sizeBytes < 1) {
+				throw new IllegalArgumentException("can't calculate size for xtype " + this);
+			}
+			// i forget if this is required, but JVM will compile it out if not...
+			return (long)count * (long)sizeBytes;
+		}
+
+		public int getCode() {
+			return code;
 		}
 
 		public static XType findXType(int xType) {
@@ -219,6 +172,34 @@ public class NCUtil {
 			} else {
 				return values()[xType];
 			}
+		}
+		
+		public static XType findXType(Object o) {
+			Class clazz = o.getClass();
+			if (clazz.isArray()) {
+				clazz = clazz.getComponentType();
+			}
+
+			if (clazz == byte.class) {
+				return NC_BYTE;
+			}
+			if (clazz == short.class) {
+				return NC_SHORT;
+			}
+			if (clazz == int.class) {
+				return NC_INT;
+			}
+			if (clazz == long.class) {
+				return NC_LONG;
+			}
+			if (clazz == float.class) {
+				return NC_DOUBLE;
+			}
+			if (clazz == double.class) {
+				return NC_LONG;
+			}
+
+			return NC_NAT;
 		}
 	}
 
